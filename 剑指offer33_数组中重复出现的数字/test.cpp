b79cf4cb8b22451d0aaddbd8a09f8320d71b17d7
浅忆梦微凉 <3538922590@qq.com>
2019-02-16 19:14:40 +0800

    new新空间并且初始化为0

--- 剑指offer33_数组中重复出现的数字/test.cpp
+++ 剑指offer33_数组中重复出现的数字/test.cpp
@@ -38,7 +38,7 @@ void TestFunc(){
 	int length = sizeof(array) / sizeof(array[0]);
 	Solution s;
 	int *duplication = nullptr;
-	duplication = new int;
+	duplication = new int(0);
 	cout << s.duplicate(array, length, duplication) << endl;
 	cout << "重复出现的数字为：" << endl;
 	cout << *duplication << endl;
